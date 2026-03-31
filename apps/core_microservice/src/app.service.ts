1e4d2c278b4acb83c7459e04e1c516662b4dbbab
Oskar Patkowski <oskar.patkowski@innowise.com>
2026-03-31 14:59:41 +0200

    add services project structure

--- apps/core_microservice/src/app.service.ts
+++ apps/core_microservice/src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const x: string = 'test';
+    console.log(x);
     return 'Hello World!';
   }
 }

a768ff3e7d642f570c0d90d8aff986163d5a1390
Oskar Patkowski <oskar.patkowski@innowise.com>
2026-04-02 12:52:29 +0200

    remove debug code, improve CI pipeline accordint to suggested changes

--- apps/core_microservice/src/app.service.ts
+++ apps/core_microservice/src/app.service.ts
@@ -3,8 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const x: string = 'test';
-    console.log(x);
     return 'Hello World!';
   }
 }

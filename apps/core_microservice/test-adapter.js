8b7d013be1176027c4d61ee54bf4f0612c03232d
Oskar <oskar.patkowski@innowise.com>
2026-04-07 23:56:41 +0200

    Delete apps/core_microservice/test-adapter.js

--- apps/core_microservice/test-adapter.js
+++
@@ -1,7 +0,0 @@
-const { PrismaPg } = require('@prisma/adapter-pg');
-try {
-  const adapter = new PrismaPg({ connectionString: 'postgres://user:pass@localhost/db' });
-  console.log("PrismaPg accepts connectionString");
-} catch (e) {
-  console.error("PrismaPg error:", e.message);
-}

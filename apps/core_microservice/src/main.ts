41383a73e091239bb3a3f7918f65d7cc33e4f367
Oskar Patkowski <oskar.patkowski@innowise.com>
2026-04-09 11:10:57 +0200

    repair config, remove wrong imports, add the right providers to modules and set up swagger

--- apps/core_microservice/src/main.ts
+++ apps/core_microservice/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Innogram core microservice')
+    .setDescription('The Innogram core microservice API')
+    .setVersion('0.1')
+    .addTag('core')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
